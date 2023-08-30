ba7c441b62e91f478a81c51f873cc549893c038d
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-08-30 18:39:05 +0100

    css the logo

--- Javascript/books.js
+++ Javascript/books.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// Array that will holds book data
+const myLibrary = [];
+
+// Stored Book information
+function Book(title, authur, page, publishedDate, readStatus) {
+  this.title = title;
+  this.authur = authur;
+  this.page = page;
+  this.publishedDate = publishedDate;
+  this.readStatus = readStatus;
+}
+// function to add  book to the array list
+function addBookToLibrary() {
+  const newBook = Book(title, authur, page, publishedDate, readStatus);
+  myLibrary.push(newBook);
+}
