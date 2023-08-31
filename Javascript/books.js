ba667f3d0f0fa63ab183b238019e28684f906fc5
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-08-31 01:01:51 +0100

    creating the populate function so that it will populate the table once the submit button is pressed

--- Javascript/books.js
+++ Javascript/books.js
@@ -21,15 +21,50 @@ function Book(title, authur, page, readStatus) {
 }
 // function to add  book to the array list
 function addBookToLibrary() {
-  const newBook = Book(bTitle, bAuthur, bPage, bStatus);
+  const newBook = new Book(
+    bTitle.value,
+    bAuthur.value,
+    bPage.value,
+    bStatus.value
+  );
   myLibrary.push(newBook);
 }
 
-function populateTable() {}
+function populateTable() {
+  const table = document.getElementById("Books-table");
+
+  // Clear existing rows
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const row = document.createElement("tr");
+
+    const titleCol = document.createElement("td");
+    titleCol.textContent = myLibrary[i].title;
+    row.appendChild(titleCol);
+
+    const authorCol = document.createElement("td");
+    authorCol.textContent = myLibrary[i].author;
+    row.appendChild(authorCol);
+
+    const pageCol = document.createElement("td");
+    pageCol.textContent = myLibrary[i].page;
+    row.appendChild(pageCol);
+
+    const readStatusCol = document.createElement("td");
+    readStatusCol.textContent = myLibrary[i].readStatus;
+    row.appendChild(readStatusCol);
+
+    table.appendChild(row);
+  }
+}
 
 submitBtn.addEventListener("click", () => {
   console.log(bTitle.value, bAuthur.value, bPage.value, bStatus.value);
   addBookToLibrary();
   addBookCon1.classList.remove("active");
   mainCon1.classList.remove("conBlur");
+  populateTable();
 });
